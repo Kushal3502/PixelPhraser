5d6a319386bf5903d31ec814ece84020e2c08078
Kushal3502 <kushal2002personal@gmail.com>
2024-12-02 20:43:34 +0530

    slider

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,85 +1,29 @@
-import { cn } from "@/lib/utils";
 import Marquee from "./ui/marquee";
+import { images } from "@/config/images";
 
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-];
+const firstRow = images.slice(0, images.length / 2);
+const secondRow = images.slice(images.length / 2);
 
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
-
-const ReviewCard = ({ img, name, username, body }) => {
+function ImageCard({ url }) {
   return (
-    <figure
-      className={cn(
-        "relative w-80 h-80 overflow-hidden rounded-xl border p-4 ",
-        // light styles
-        "border-gray-400/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-400/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
-      )}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
-    </figure>
+    <div className="md:w-96 md:h-80 w-80 h-64 mx-4">
+      <img src={url} className="h-full w-full object-cover rounded-lg shadow-lg" />
+    </div>
   );
-};
+}
 
 function Slider() {
   return (
-    <div className="absolute inset-0 opacity-60 z-0 top-32">
-      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg md:shadow-xl">
-        <Marquee className="[--duration:20s] mb-8">
-          {firstRow.map((review) => (
-            <ReviewCard key={review.username} {...review} />
+    <div className="absolute inset-0 z-0 opacity-15 top-4 lg:top-16">
+      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg shadow-xl">
+        <Marquee className="[--duration:70s] mb-8 overflow-hidden">
+          {firstRow.map((item, index) => (
+            <ImageCard key={index} url={item} />
           ))}
         </Marquee>
-        <Marquee reverse className="[--duration:20s]">
-          {secondRow.map((review) => (
-            <ReviewCard key={review.username} {...review} />
+        <Marquee reverse className="[--duration:70s]">
+          {secondRow.map((item, index) => (
+            <ImageCard key={index} url={item} />
           ))}
         </Marquee>
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-gray-950 dark:from-background"></div>
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -3,15 +3,18 @@ import BlurIn from "@/components/ui/blur-in";
 import { RainbowButton } from "@/components/ui/rainbow-button";
 import SparklesText from "@/components/ui/sparkles-text";
 import TypingAnimation from "@/components/ui/typing-animation";
+import { images } from "@/config/images";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const navigate = useNavigate();
 
+  console.log(images.length);
+
   return (
     <div className="relative h-full my-auto flex flex-col justify-center items-center text-center px-4 py-16 ">
-      {/* <Slider /> */}
+      <Slider />
       <div className="relative z-10 max-w-4xl mx-auto space-y-6">
         <SparklesText
           text="PixelPhraser"
