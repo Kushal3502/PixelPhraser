c0e7141733521f5f6c03d224acded70f639fcf61
Kushal3502 <kushal2002personal@gmail.com>
2024-12-02 14:55:36 +0530

    gh

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,12 +11,13 @@ const port = process.env.PORT || 3000;
 
 // initialize app
 const app = express();
-console.log(process.env.CORS)
+
 // middlewares
 app.use(
   cors({
     origin: [process.env.CORS, "http://localhost:5173"],
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    credentials: true, // Include credentials if needed
   })
 );
 
